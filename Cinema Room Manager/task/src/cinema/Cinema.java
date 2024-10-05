d98a9162c23e50a148b90065fec3489936e4583e
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-10-05 11:36:48 +0200

    Step 1 finished, added graphical representation of theatre's seating.

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,5 +4,11 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
+        System.out.println("Cinema:");
+        System.out.println(" 1 2 3 4 5 6 7 8");
+        for(int i = 1; i < 8; i++){
+            String str = String.format("%d %s", i, "S S S S S S S S");
+            System.out.println(str);
+        }
     }
 }
